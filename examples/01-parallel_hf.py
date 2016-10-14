f9947e98ad81fbf26b907dcbf8323a4e60ee6195
Qiming Sun <osirpt.sun@gmail.com>
2016-10-14 01:39:27 -0700

    Add example

---
+++ examples/01-parallel_hf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#
+# Author: Qiming Sun <osirpt.sun@gmail.com>
+#
+
+'''
+Parallelize gamma point
+'''
+
+import numpy
+import pyscf.pbc.gto as pbcgto
+import pyscf.pbc.scf as pscf
+from mpi4pyscf.pbc import df as mpidf
+
+cell = pbcgto.Cell()
+cell.atom = [['C', ([ 0.,  0.,  0.])],
+             ['C', ([ 0.8917,  0.8917,  0.8917])],
+             ['C', ([ 1.7834,  1.7834,  0.    ])],
+             ['C', ([ 2.6751,  2.6751,  0.8917])],
+             ['C', ([ 1.7834,  0.    ,  1.7834])],
+             ['C', ([ 2.6751,  0.8917,  2.6751])],
+             ['C', ([ 0.    ,  1.7834,  1.7834])],
+             ['C', ([ 0.8917,  2.6751,  2.6751])]
+            ]
+cell.h = numpy.eye(3) * 3.5668
+cell.basis = 'sto3g'
+cell.gs = [5] * 3
+cell.verbose = 4
+cell.build()
+cell.nimgs = [2]*3
+cell.max_memory = 1
+
+mydf = mpidf.MDF(cell)
+mydf.auxbasis = 'weigend'
+mydf.metric = 'T'
+mydf.approx_sr_level = 0
+
+mf = pscf.RHF(cell)
+mf.exxdiv = 'ewald'
+mf.with_df = mydf
+mf.kernel()
